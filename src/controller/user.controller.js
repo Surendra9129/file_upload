4176ed4ca40279ed6ff1d88dc34f4ca3184f5673
Surendra9129 <91534930+Surendra9129@users.noreply.github.com>
2021-12-02 17:41:36 +0530

    completed pagination assignment

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,16 @@
 const express=require('express');
 const router=express.Router();
 const user=require('../model/user.model');
+const upload=require('../middleware/upload');
 
-router.use('/',async(req,res)=>{
-    const new_user=await user.create(req.body);
-    res.status(201).send({new_user});
+router.post('/', upload.any('product_image'),async(req,res)=>{
+    const filePath=req.files.map((file)=>file.path);
+    const new_user=await user.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        profile_pic: filePath
+    })
+   return  res.status(201).json({new_user});
 });
 
 router.use('/',async(req,res)=>{
--- src/server.js
+++ src/server.js
@@ -7,7 +7,12 @@ const connect=require('./configs/db');
 const user_controller=require('./controller/user.controller');
 app.use('/users',user_controller);
 
-app.listen(port,async(req,res)=>{
-    await connect();
-console.log(`running on port ${port}`);
-});
+
+const start=()=>{
+    app.listen(port,async(req,res)=>{
+        await connect();
+    console.log(`running on port ${port}`);
+    });
+}
+
+module.exports=start;
